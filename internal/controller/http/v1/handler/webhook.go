9ba10b43300ca723a27711b3189914068901c21f
Dilshod <dilshodforeverr@gmail.com>
2025-04-07 16:28:31 +0500

    add

--- internal/controller/http/v1/handler/webhook.go
+++ internal/controller/http/v1/handler/webhook.go
@@ -43,7 +43,7 @@ func (h Handler) WebhookHandler(c *gin.Context) {
 	// Agar mode 'subscribe' bo'lsa, challenge qaytarish
 	if mode == "subscribe" {
 		fmt.Println("resp in subscribe")
-		c.JSON(http.StatusOK, gin.H{"hub.challenge": challenge})
+		c.JSON(http.StatusOK, gin.H{"status": "success"})
 		return
 	}
 
